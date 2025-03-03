aaf502a6b0171b5426518e5ee7c0b571dbda5d7b
Yuma Shintani <yuu7taku@icloud.com>
2025-03-04 01:04:07 +0900

    feat open graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,7 @@ import { LinkCardComponent } from "./src/plugins/rehype-component-linkcard.mjs";
 import { parseDirectiveNode } from "./src/plugins/remark-directive-rehype.js";
 import { remarkExcerpt } from "./src/plugins/remark-excerpt.js";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import { rawFonts } from './src/plugins/vite-raw-fonts.mjs';
 
 import expressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
@@ -131,5 +132,6 @@ export default defineConfig({
         },
       },
     },
+    plugns:[rawFonts(['.woff2', '.ttf', '.woff', '.otf'])]
   },
 });
